69f15afd4184bbfe3a462ad220040893257a4d43
Sai Akshith V <akshith292@gmail.com>
2022-07-25 20:34:44 +0530

    updated footer url

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 const Footer = () => {
 
     return (
         <footer className="footer p-7 bg-gray-800 text-primary-content footer-center">
             <div>
-                <Link to="https://github.com/akshith2426/competitive-finder" target="_blank">View source code 🚀 </Link>
+                <a href="https://github.com/akshith2426/competitive-finder" target="_blank">View source code 🚀 </a>
             </div>
         </footer>
     )
